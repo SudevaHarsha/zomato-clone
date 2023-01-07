2b279065de4455011eed97d4fbb27fd6ffccaf1e
SudevaHarsha <sudevaharsha1@gmail.com>
2023-01-07 11:52:14 +0530

    food schema

---
+++ server/database/allmodels.js
@@ -0,0 +1,17 @@
+import {FoodModel} from "./food";
+import {ImageModel} from "./image";
+import {ResturantModel} from "./resturant";
+import {MenuModel} from "./menu";
+import {OrderModel} from "./order";
+import {ReviewModel} from "./review";
+import {UserModel} from "./user";
+
+export{
+    FoodModel,
+    ResturantModel,
+    ImageModel,
+    MenuModel,
+    OrderModel,
+    ReviewModel,
+    UserModel
+}
---
+++ server/database/food/index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const FoodSchema = new mongoose.Schema(
+    {
+        name: {type : String,required : true},
+        descript:{type : String, required : true},
+        isVeg:{type : Boolean, required : true},
+        isContainsEggs:{type : Boolean, required : true},
+        category:{tyoe : String, required : true},
+        photos:{
+            type: mongoose.Types.ObjectId,
+            ref: "Images",
+        },
+        price: {type: Number, default: 150, required : true},
+        addOns: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref:"foods"
+            }
+        ],
+        resturant:{
+            ref:"resturants",
+            required: true
+        }   
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const FoodModel = mongoose.model("foods",FoodSchema);
